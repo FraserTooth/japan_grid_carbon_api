b437bd280f54924115f959a1ce2e95175072b0ae
Fraser Tooth <frasertooth@gmail.com>
2020-01-09 17:38:14 +0900

    Minor API Update

--- japan_grid_carbon_api/views.py
+++ japan_grid_carbon_api/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return HttpResponse("Hello There, this will be an API for Calculating the Carbon Intensity of the Japanese Grid!")
