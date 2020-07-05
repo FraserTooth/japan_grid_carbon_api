54edfa5ca6ee5ab20ed9cb59d338b8f88eef0b51
Fraser Tooth <frasertooth@gmail.com>
2020-07-05 23:24:30 +0900

    Auth path added

--- local.sh
+++ local.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 COMPANY=$1
 FUNCTION=$2
+DIR="$PWD"
+
+export GOOGLE_APPLICATION_CREDENTIALS="$DIR/gc-key.json"
 
 echo "Locally Running Function $FUNCTION for $COMPANY"
 cd cloud_functions/$COMPANY/scraper/
