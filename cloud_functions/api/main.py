e044ef6a4aba73d2bdb5282ba0b155f1cf2c51d2
Fraser Tooth <frasertooth@gmail.com>
2020-07-22 15:59:55 +0900

    Added daily by month breakdown

--- cloud_functions/api/main.py
+++ cloud_functions/api/main.py
@@ -34,6 +34,33 @@ def _extract_daily_info_from_big_query(utility):
     return pd.read_gbq(query)
 
 
+def _extract_daily_info_by_month_from_big_query(utility):
+
+    query = """
+    SELECT
+        EXTRACT(MONTH FROM datetime) AS month,
+        EXTRACT(HOUR FROM datetime) AS hour,
+        AVG(kWh_demand) as kWh_demand_avg,
+        AVG(kWh_nuclear) as kWh_nuclear_avg,
+        AVG(kWh_fossil) as kWh_fossil_avg,
+        AVG(kWh_hydro) as kWh_hydro_avg,
+        AVG(kWh_geothermal) as kWh_geothermal_avg,
+        AVG(kWh_biomass) as kWh_biomass_avg,
+        AVG(kWh_solar_output) as kWh_solar_output_avg,
+        AVG(kWh_solar_throttling) as kWh_solar_throttling_avg,
+        AVG(kWh_wind_output) as kWh_wind_output_avg,
+        AVG(kWh_wind_throttling) as kWh_wind_throttling_avg,
+        AVG(kWh_pumped_storage) as kWh_pumped_storage_avg,
+        AVG(kWh_interconnectors) as kWh_interconnectors_avg,
+        AVG(kWh_total) as kWh_total_avg
+    FROM `japan-grid-carbon-api.{utility}.historical_data_by_generation_type`
+    GROUP BY month, hour
+    order by month, hour asc
+    """.format(utility=utility)
+
+    return pd.read_gbq(query)
+
+
 def _tepco_daily_intensity():
     df = tci.addCarbonIntensityFactors(
         _extract_daily_info_from_big_query("tepco"), "_hour")
@@ -42,12 +69,15 @@ def _tepco_daily_intensity():
     return df.to_json(orient='index', date_format="iso")
 
 
+def _tepco_daily_intensity_by_month():
+    df = tci.addCarbonIntensityFactors(
+        _extract_daily_info_by_month_from_big_query("tepco"), "_avg")
+
+    df.reset_index(inplace=True)
+    return df.to_json(orient='index', date_format="iso")
+
+
 def daily_carbon_intensity(request):
-
-    # For more information about CORS and CORS preflight requests, see
-    # https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request
-    # for more information.
-
     # Set CORS headers for the preflight request
     if request.method == 'OPTIONS':
         # Allows GET requests from any origin with the Content-Type
@@ -78,3 +108,36 @@ def daily_carbon_intensity(request):
 
     if utility == "tepco":
         return _tepco_daily_intensity(), 200, headers
+
+
+def daily_carbon_intensity_by_month(request):
+    # Set CORS headers for the preflight request
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'utility' in request_json:
+        utility = request_json['utility']
+    elif request_args and 'utility' in request_args:
+        utility = request_args['utility']
+    else:
+        return f'No utility specified', 400, headers
+
+    if utility == "tepco":
+        return _tepco_daily_intensity_by_month(), 200, headers
