920d1191f6ab15db4d3a3f5b1f8b474acf5de8af
Fraser Tooth <frasertooth@gmail.com>
2020-07-23 23:57:57 +0900

    Applied direct carbon intensity evaluation on BQ side, also fixed cache

--- cloud_functions/api/main.py
+++ cloud_functions/api/main.py
@@ -49,6 +49,45 @@ def _extract_daily_carbon_intensity_from_big_query(utility):
     return pd.read_gbq(query)
 
 
+def _extract_daily_carbon_intensity_by_month_from_big_query(utility):
+
+    carbonIntensity = tci.getCarbonIntensityFactors()
+
+    query = """
+    SELECT
+    EXTRACT(MONTH FROM datetime) AS month,
+    EXTRACT(HOUR FROM datetime) AS hour,
+    AVG((
+        (kWh_nuclear * {intensity_nuclear}) + 
+        (kWh_fossil * {intensity_fossil}) + 
+        (kWh_hydro * {intensity_hydro}) + 
+        (kWh_geothermal * {intensity_geothermal}) + 
+        (kWh_biomass * {intensity_biomass}) +
+        (kWh_solar_output * {intensity_solar_output}) +
+        (kWh_wind_output * {intensity_wind_output}) +
+        (kWh_pumped_storage * {intensity_pumped_storage}) +
+        (kWh_interconnectors * {intensity_interconnectors}) 
+        ) / kWh_total
+        ) as carbon_intensity
+    FROM japan-grid-carbon-api.{utility}.historical_data_by_generation_type
+    GROUP BY month, hour
+    order by month, hour asc
+    """.format(
+        utility=utility,
+        intensity_nuclear=carbonIntensity["kWh_nuclear"],
+        intensity_fossil=carbonIntensity["kWh_fossil"],
+        intensity_hydro=carbonIntensity["kWh_hydro"],
+        intensity_geothermal=carbonIntensity["kWh_geothermal"],
+        intensity_biomass=carbonIntensity["kWh_biomass"],
+        intensity_solar_output=carbonIntensity["kWh_solar_output"],
+        intensity_wind_output=carbonIntensity["kWh_wind_output"],
+        intensity_pumped_storage=carbonIntensity["kWh_pumped_storage"],
+        intensity_interconnectors=carbonIntensity["kWh_interconnectors"]
+    )
+
+    return pd.read_gbq(query)
+
+
 def _extract_daily_info_from_big_query(utility):
 
     query = """
@@ -128,15 +167,22 @@ def _tepco_daily_intensity_by_month():
         print("Returning cache._tepco_daily_intensity_by_month:")
         return cache["_tepco_daily_intensity_by_month"]
 
-    df = tci.addCarbonIntensityFactors(
-        _extract_daily_info_by_month_from_big_query("tepco"), "_avg")
+    df = _extract_daily_carbon_intensity_by_month_from_big_query("tepco")
 
     df.reset_index(inplace=True)
 
-    output = df.groupby('month').agg({"carbon_intensity": list}).to_dict()
+    output = {
+        "carbon_intensity_by_month": df.groupby('month')
+        .apply(
+            lambda month: month[['hour', 'carbon_intensity']]
+            .to_dict(orient='records')
+        ).to_dict(),
+        "fromCache": True
+    }
 
     # Populate Cache
     cache['_tepco_daily_intensity_by_month'] = copy.deepcopy(output)
+    output["fromCache"] = False
     return output
 
 
