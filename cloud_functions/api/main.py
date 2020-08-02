baac80c6dbf75c74c4a35c01e0f78195785afedb
Fraser Tooth <frasertooth@gmail.com>
2020-08-02 19:07:12 +0900

    Started to Unify Endpoints

--- cloud_functions/api/main.py
+++ cloud_functions/api/main.py
@@ -1,10 +1,54 @@
 import json
+import werkzeug.datastructures
+from flask import Flask
+app = Flask(__name__)
 
+from pprint import pprint
 
 import tepco.api as tepco
 
+# Add CORS to All Requests
+headers = {
+    'Access-Control-Allow-Origin': '*'
+}
 
-def dailyCarbon(request):
+
+@app.route('/home/<name>')
+def home(name):
+    return "hello," + name
+
+
+def main(request):
+    with app.app_context():
+        headers = werkzeug.datastructures.Headers()
+        for key, value in request.headers.items():
+            headers.add(key, value)
+        with app.test_request_context(method=request.method, base_url=request.base_url, path=request.path, query_string=request.query_string, headers=headers, data=request.data):
+            try:
+                rv = app.preprocess_request()
+                if rv is None:
+                    rv = app.dispatch_request()
+            except Exception as e:
+                rv = app.handle_user_exception(e)
+            response = app.make_response(rv)
+            return app.process_response(response)
+
+
+@app.route('/daily_carbon_intensity/<utility>')
+def daily_carbon_intensity(utility):
+    response = {}
+    utilities = {
+        "tepco": tepco.daily_intensity,
+    }
+    response['data'] = utilities.get(utility, lambda: None)()
+
+    if response['data'] == None:
+        return f'Invalid utility specified', 400, headers
+
+    return json.dumps(response), 200, headers
+
+
+def daily_carbon_intensity(request):
     # Set CORS headers for the preflight request
     if request.method == 'OPTIONS':
         # Allows GET requests from any origin with the Content-Type
