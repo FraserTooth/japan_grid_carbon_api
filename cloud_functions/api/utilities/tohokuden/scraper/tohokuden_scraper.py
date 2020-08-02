cad9a34c3f235f9f94728464dcef7b2bca8013b5
Fraser Tooth <frasertooth@gmail.com>
2020-08-02 22:58:59 +0900

    Scraper Parts Done@

--- cloud_functions/api/utilities/tohokuden/scraper/tohokuden_scraper.py
+++ cloud_functions/api/utilities/tohokuden/scraper/tohokuden_scraper.py
@@ -8,22 +8,19 @@ import datetime
 def _renameHeader(header):
 
     translations = {
-        "Unnamed: 0": "date",
-        "Unnamed: 1": "time",
-        "Unnamed: 0_Unnamed: 1": "datetime",
-        "Unnamed: 2": "kWh_demand",
-        "原子力": "kWh_nuclear",
-        "火力": "kWh_fossil",
-        "水力": "kWh_hydro",
-        "地熱": "kWh_geothermal",
-        "バイオマス": "kWh_biomass",
-        "太陽光発電実績": "kWh_solar_output",
-        "太陽光出力制御量": "kWh_solar_throttling",
-        "風力発電実績": "kWh_wind_output",
-        "風力出力制御量": "kWh_wind_throttling",
-        "揚水": "kWh_pumped_storage",
-        "連系線": "kWh_interconnectors",
-        "合計": "kWh_total"
+        "DATE_TIME": "datetime",
+        "エリア需要〔MWh〕": "kWh_area_demand",
+        "水力〔MWh〕": "kWh_hydro",
+        "火力〔MWh〕": "kWh_fossil",
+        "原子力〔MWh〕": "kWh_nuclear",
+        "太陽光実績〔MWh〕": "kWh_solar_output",
+        "太陽光抑制量〔MWh〕": "kWh_solar_throttling",
+        "風力実績〔MWh〕": "kWh_wind_output",
+        "風力抑制量〔MWh〕": "kWh_wind_throttling",
+        "地熱〔MWh〕": "kWh_geothermal",
+        "バイオマス〔MWh〕": "kWh_biomass",
+        "揚水〔MWh〕": "kWh_pumped_storage",
+        "連系線〔MWh〕": "kWh_interconnectors",
     }
 
     if header in translations:
@@ -32,39 +29,59 @@ def _renameHeader(header):
 
 
 def _parseTohokudenCsvs():
-    CSV_URL_DAILY = 'https://www.tepco.co.jp/forecast/html/images/juyo-d-j.csv'
 
-    CSV_URLS = [
-        'http://www.tepco.co.jp/forecast/html/images/area-2016.csv',
-        'http://www.tepco.co.jp/forecast/html/images/area-2017.csv',
-        'http://www.tepco.co.jp/forecast/html/images/area-2018.csv',
-        'http://www.tepco.co.jp/forecast/html/images/area-2019.csv',
-        'http://www.tepco.co.jp/forecast/html/images/area-2020.csv'
-    ]
+    CSV_URLS = []
+
+    for year in range(2016, 2021):
+        for quarter in range(1, 5):
+            url = "https://setsuden.nw.tohoku-epco.co.jp/common/demand/juyo_{year}_tohoku_{quarter}Q.csv".format(
+                year=year,
+                quarter=quarter
+            )
+            CSV_URLS.append(url)
+
+    # DATE_TIME,
+    # エリア需要〔MWh〕,
+    # 水力〔MWh〕,
+    # 火力〔MWh〕,
+    # 原子力〔MWh〕,
+    # 太陽光実績〔MWh〕,
+    # 太陽光抑制量〔MWh〕,
+    # 風力実績〔MWh〕,
+    # 風力抑制量〔MWh〕,
+    # 地熱〔MWh〕,
+    # バイオマス〔MWh〕,
+    # 揚水〔MWh〕,
+    # 連系線〔MWh〕
 
     dtypes = {
-        "Unnamed: 2": int,
-        "火力": int,
-        "水力": int,
-        "地熱": int,
-        "バイオマス": int,
-        "太陽光発電実績": int,
-        "太陽光出力制御量": int,
-        "風力発電実績": int,
-        "風力出力制御量": int,
-        "揚水": int,
-        "連系線": int,
-        "合計": int
+        "エリア需要〔MWh〕": int,
+        "水力〔MWh〕": int,
+        "火力〔MWh〕": int,
+        "原子力〔MWh〕": int,
+        "太陽光実績〔MWh〕": int,
+        "太陽光抑制量〔MWh〕": int,
+        "風力実績〔MWh〕": int,
+        "風力抑制量〔MWh〕": int,
+        "地熱〔MWh〕": int,
+        "バイオマス〔MWh〕": int,
+        "揚水〔MWh〕": int,
+        "連系線〔MWh〕": int
     }
 
-    def _getTEPCOCSV(url):
+    def _getTohokudenCSV(url):
         print("  -- getting:", url)
-        return pd.read_csv(url, skiprows=2, encoding="cp932",
-                           parse_dates=[[0, 1]], dtype=dtypes, thousands=",")
+        try:
+            data = pd.read_csv(url, skiprows=0, encoding="cp932", dtype=dtypes)
+        except Exception as e:
+            print("Caught error \"{error}\" at {url}".format(
+                error=e, url=url))
+            return None
+        return data
 
     print("Reading CSVs")
 
-    dataList = map(_getTEPCOCSV, CSV_URLS)
+    dataList = map(_getTohokudenCSV, CSV_URLS)
 
     df = pd.concat(dataList)
 
@@ -76,7 +93,7 @@ def _parseTohokudenCsvs():
 
 
 def get_tohokuden_as_json():
-    df = _parseTepcoCsvs()
+    df = _parseTohokudenCsvs()
     return df.to_json(orient='index', date_format="iso")
 
 
@@ -86,7 +103,7 @@ def convert_tohokuden_df_to_json(df):
 
 
 def get_tohokuden_as_csv():
-    df = _parseTepcoCsvs()
+    df = _parseTohokudenCsvs()
     return df.to_csv(index=False)
 
 
@@ -95,4 +112,7 @@ def convert_tohokuden_df_to_csv(df):
 
 
 def get_tohokuden_as_dataframe():
-    return _parseTepcoCsvs()
+    return _parseTohokudenCsvs()
+
+
+print(get_tohokuden_as_dataframe().tail())
