8cc53ddcbd53c2a41fcac3f4d299310063979bd4
Fraser Tooth <frasertooth@gmail.com>
2020-08-02 21:00:41 +0900

    Combined Breakdown Endpoints

--- cloud_functions/api/main.py
+++ cloud_functions/api/main.py
@@ -13,9 +13,11 @@ headers = {
 }
 
 
-@app.route('/home/<name>')
-def home(name):
-    return "hello," + name
+def selectUtility(utility):
+    utilities = {
+        "tepco": tepco,
+    }
+    return utilities.get(utility, None)
 
 
 def main(request):
@@ -37,120 +39,35 @@ def main(request):
 @app.route('/daily_carbon_intensity/<utility>')
 def daily_carbon_intensity(utility):
     response = {}
-    utilities = {
-        "tepco": tepco.daily_intensity,
-    }
-    response['data'] = utilities.get(utility, lambda: None)()
 
-    if response['data'] == None:
+    utilityClass = selectUtility(utility)
+
+    if utilityClass == None:
         return f'Invalid utility specified', 400, headers
+
+    response['data'] = utilityClass.daily_intensity()
 
     return json.dumps(response), 200, headers
 
 
-def daily_carbon_intensity(request):
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600'
-        }
-
-        return ('', 204, headers)
-
-    # Set CORS headers for the main request
-    headers = {
-        'Access-Control-Allow-Origin': '*'
-    }
-
-    request_json = request.get_json(silent=True)
-    request_args = request.args
-
+@app.route('/daily_carbon_intensity/<utility>/<breakdown>')
+def daily_carbon_intensity_with_breakdown(utility, breakdown):
     response = {}
 
-    if request_json and 'utility' in request_json:
-        utility = request_json['utility']
-    elif request_args and 'utility' in request_args:
-        utility = request_args['utility']
-    else:
-        return f'No utility specified', 400, headers
+    utilityClass = selectUtility(utility)
 
-    if utility == "tepco":
-        response['data'] = tepco.daily_intensity()
-        return json.dumps(response), 200, headers
+    if utilityClass == None:
+        return f'Invalid utility specified', 400, headers
 
-
-def daily_carbon_intensity_by_month(request):
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600'
-        }
-
-        return ('', 204, headers)
-
-    # Set CORS headers for the main request
-    headers = {
-        'Access-Control-Allow-Origin': '*'
+    breakdowns = {
+        "month": utilityClass.daily_intensity_by_month,
+        "month_and_weekday": utilityClass.daily_intensity_by_month_and_weekday
     }
+    dataSource = breakdowns.get(breakdown, None)
 
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+    if dataSource == None:
+        return f'Invalid Breakdown Specified', 400, headers
 
-    response = {}
+    response['data'] = dataSource()
 
-    if request_json and 'utility' in request_json:
-        utility = request_json['utility']
-    elif request_args and 'utility' in request_args:
-        utility = request_args['utility']
-    else:
-        return f'No utility specified', 400, headers
-
-    if utility == "tepco":
-        response['data'] = tepco.daily_intensity_by_month()
-        return response, 200, headers
-
-
-def daily_carbon_intensity_by_month_and_weekday(request):
-    # Set CORS headers for the preflight request
-    if request.method == 'OPTIONS':
-        # Allows GET requests from any origin with the Content-Type
-        # header and caches preflight response for an 3600s
-        headers = {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Max-Age': '3600'
-        }
-
-        return ('', 204, headers)
-
-    # Set CORS headers for the main request
-    headers = {
-        'Access-Control-Allow-Origin': '*'
-    }
-
-    request_json = request.get_json(silent=True)
-    request_args = request.args
-
-    response = {}
-
-    if request_json and 'utility' in request_json:
-        utility = request_json['utility']
-    elif request_args and 'utility' in request_args:
-        utility = request_args['utility']
-    else:
-        return f'No utility specified', 400, headers
-
-    if utility == "tepco":
-        response['data'] = tepco.daily_intensity_by_month_and_weekday()
-        return response, 200, headers
+    return json.dumps(response), 200, headers
