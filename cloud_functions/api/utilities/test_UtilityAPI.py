c6fbf88e68def61a99b6f4a0da44f74a76154f96
Fraser Tooth <frasertooth@gmail.com>
2020-08-16 18:33:54 +0900

    Daily intensity test query string test added

--- cloud_functions/api/utilities/test_UtilityAPI.py
+++ cloud_functions/api/utilities/test_UtilityAPI.py
@@ -48,3 +48,28 @@ def test_get_carbon_intensity_factors(requests_mock):
     }
 
     assert expected == api.get_carbon_intensity_factors()
+
+
+def test_daily_intensity(requests_mock):
+    api = UtilityAPI('tepco')
+
+    requests_mock.get(
+        "https://api.carbonintensity.org.uk/intensity/factors", text=uk_carbon_intensity_response)
+
+    expected = """
+        AVG((
+            (kWh_nuclear * 0) + 
+            (kWh_fossil * 741.68489817766) + 
+            (kWh_hydro * 0) + 
+            (kWh_geothermal * 0) + 
+            (kWh_biomass * 120) +
+            (kWh_solar_output * 0) +
+            (kWh_wind_output * 0) +
+            (kWh_pumped_storage * 0) +
+            (if(kWh_interconnectors > 0,kWh_interconnectors, 0) * 500) 
+            ) / kWh_total
+            ) as carbon_intensity
+        FROM japan-grid-carbon-api.tepco.historical_data_by_generation_type
+        """
+
+    assert expected == api._get_intensity_query_string()
