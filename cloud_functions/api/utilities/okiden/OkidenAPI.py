5a644b50c39722b726f00cc71d3991b57e9ef243
Fraser Tooth <frasertooth@gmail.com>
2020-10-07 00:22:10 +0900

    OkidenAPI bugfix

--- cloud_functions/api/utilities/okiden/OkidenAPI.py
+++ cloud_functions/api/utilities/okiden/OkidenAPI.py
@@ -6,7 +6,7 @@ from ..UtilityAPI import UtilityAPI
 
 class OkidenAPI(UtilityAPI):
     def __init__(self):
-        super().__init__("cepco")
+        super().__init__("okiden")
 
     def _get_intensity_query_string(self):
         ci = self.get_carbon_intensity_factors()
@@ -15,7 +15,6 @@ class OkidenAPI(UtilityAPI):
         AVG((
             (MWh_fossil * {intensity_fossil}) + 
             (MWh_hydro * {intensity_hydro}) + 
-            (MWh_geothermal * {intensity_geothermal}) + 
             (MWh_biomass * {intensity_biomass}) +
             (MWh_solar_output * {intensity_solar_output}) +
             (MWh_wind_output * {intensity_wind_output})
@@ -23,14 +22,13 @@ class OkidenAPI(UtilityAPI):
             ) as carbon_intensity
         FROM (
             SELECT *,
-            (MWh_fossil + MWh_hydro + MWh_geothermal + MWh_biomass + MWh_solar_output + MWh_wind_output) as MWh_total_generation
+            (MWh_fossil + MWh_hydro + MWh_biomass + MWh_solar_output + MWh_wind_output) as MWh_total_generation
             FROM `japan-grid-carbon-api.{utility}.historical_data_by_generation_type`
         )
         """.format(
             utility=self.utility,
             intensity_fossil=ci["kWh_fossil"],
             intensity_hydro=ci["kWh_hydro"],
-            intensity_geothermal=ci["kWh_geothermal"],
             intensity_biomass=ci["kWh_biomass"],
             intensity_solar_output=ci["kWh_solar_output"],
             intensity_wind_output=ci["kWh_wind_output"],
