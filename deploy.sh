7f764c673e691d06c58c39eb72707c9aa996c1f0
Fraser Tooth <frasertooth@gmail.com>
2020-11-08 23:48:39 +0900

    Deploy comments

--- deploy.sh
+++ deploy.sh
@@ -17,20 +17,24 @@ else
     exit 1
 fi
 
-echo "Deploying All on $STAGE"
+echo ">> Deploying All to $STAGE with project: $PROJECT"
 cd cloud_functions/
 
 # Set Policy for Main API to Unauthenticated
 gcloud functions set-iam-policy --region=us-central1 api policy.json
 
+echo ">> Deploying Cloud Function"
 sls deploy --stage $STAGE --id $PROJECT
 
+echo ">> Deploying API Spec"
 gcloud endpoints services deploy $SPECPATH --project $PROJECT
 
 CONFIG_ID=$(gcloud endpoints configs list --service $ENDPOINT --project $PROJECT | grep -oP -m 1 '^\d\d\d\d-\d\d-\S+')
 
+echo ">> Building ESP-v2 Image"
 ../gcloud_build_image -s $ENDPOINT -c $CONFIG_ID -p $PROJECT
 
+echo ">> Deploying ESP-v2 Image"
 gcloud run deploy api \
 --image="gcr.io/$PROJECT/endpoints-runtime-serverless:2.20.0-$ENDPOINT-$CONFIG_ID" \
 --set-env-vars=ESPv2_ARGS=--cors_preset=basic \
